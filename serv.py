6de6dccc8fb938ac42713b7464451f62bd6b178e
Marc-Velay <marc.velay@gmail.com>
2019-03-03 11:04:55 +0100

    querry server

--- serv.py
+++ serv.py
@@ -1,18 +1,48 @@
 import tornado.ioloop
 import tornado.web
 import socket
+from urllib.parse import urlparse
+from neo4j import GraphDatabase
+from collections import Counter
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
 
-application = tornado.web.Application([
-    (r"/", MainHandler),
-])
+class HelloWorldExample(object):
 
-if __name__ == "__main__":
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(8888)
-    myIP = socket.gethostbyname('0.0.0.0')
-    print('*** Websocket Server Started at %s***' % myIP)
-    tornado.ioloop.IOLoop.current().start()
+    def __init__(self, uri, user, password):
+        self._driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self._driver.close()
+
+    def print_greeting(self, message):
+        with self._driver.session() as session:
+            querry = session.write_transaction(self._get_pathologies, message)
+            print(querry)
+
+    @staticmethod
+    def _get_pathologies(tx, name_list):
+        res = []
+        for name in name_list:
+            for result in tx.run("MATCH (a:Sym)-[r]->(f) "
+                             "WHERE a.name = {name} "
+                             "RETURN f.name, f.phenotype, r.strength", name=name):
+                res.append((result[0]+' '+result[1], result[2]))
+
+        counter = Counter([r[0] for r in res])
+        if len(name_list) > 1:
+            names = [name for name in counter if counter[name] > 1]
+            res = [r for r in res if any(name in r for name in names)]
+            strengths = [float(0)]*len(names)
+            r_final = [0]*len(names)
+            for r in res:
+                fl = float(r[1].replace(',', '.'))
+                if fl>strengths[names.index(r[0])]:
+                    strengths[names.index(r[0])] = fl
+                    r_final[names.index(r[0])] = r
+        else:
+            r_final = res
+        return r_final
+
+db = HelloWorldExample("bolt://134.157.26.238:7687", user="neo4j", password="toto")
+list_sym = ["Leucopenie", "?deme"]
+db.print_greeting(list_sym)
